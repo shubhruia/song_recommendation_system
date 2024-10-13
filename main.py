05b0d3a58086682126380cb88e0fcd86cf665da9
Shubh Ruia <142161559+shubhruia@users.noreply.github.com>
2024-10-14 03:54:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,15 +2,8 @@ import streamlit as st
 import requests
 import base64
 import pandas as pd
-import matplotlib.pyplot as plt
-import numpy as np
-import plotly.express as px
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.metrics.pairwise import cosine_similarity
-from datetime import datetime
-from concurrent.futures import ThreadPoolExecutor
 
 # Spotify Credentials
 CLIENT_ID = 'be065b53d379455887640cdd3b887ae6'
@@ -25,128 +18,181 @@ def get_access_token():
     }
     response = requests.post(token_url, data={'grant_type': 'client_credentials'}, headers=headers)
 
-    return response.json()['access_token'] if response.status_code == 200 else st.error(f"Error obtaining access token: {response.status_code}")
-
-# Batch function to get audio features and artist genres
-def get_audio_features_and_genres(sp, track_ids, artist_ids):
-    audio_features = sp.audio_features(tracks=track_ids)
-    artist_genres = {}
-    for i in range(0, len(artist_ids), 50):
-        artists_info = sp.artists(artist_ids[i:i+50])['artists']
-        artist_genres.update({artist['id']: artist.get('genres', []) for artist in artists_info})
-    return audio_features, artist_genres
-
-# Function to get playlist data
-def get_trending_playlist_data(playlist_id, access_token):
-    sp = spotipy.Spotify(auth=access_token)
-    playlist_tracks = sp.playlist_tracks(playlist_id, fields='items(track(id, name, artists, album(id, name, images)))')['items']
-    
-    track_ids, artist_ids = [track['track']['id'] for track in playlist_tracks], {artist['id'] for track in playlist_tracks for artist in track['track']['artists']}
-    audio_features, artist_genres = get_audio_features_and_genres(sp, track_ids, list(artist_ids))
-    
-    def get_track_data(i, track_info):
-        track = track_info['track']
-        track_data = {
-            'Track Name': track['name'],
-            'Artists': ', '.join([artist['name'] for artist in track['artists']]),
-            'Album Name': track['album']['name'],
-            'Album Image URL': track['album']['images'][0]['url'] if track['album']['images'] else None,
-            'Release Date': sp.album(track['album']['id']).get('release_date') if track['album']['id'] else None,
-            'Popularity': sp.track(track['id']).get('popularity') if track['id'] else None,
-            'Genre': ', '.join(artist_genres.get(track['artists'][0]['id'], ['Unknown'])),
-            **{key: audio_features[i][key] for key in ['danceability', 'energy', 'key', 'loudness', 'speechiness', 'acousticness', 'instrumentalness', 'liveness', 'valence', 'tempo']}
-        }
-        return track_data
-    
-    return pd.DataFrame([get_track_data(i, track_info) for i, track_info in enumerate(playlist_tracks)])
-
-# Function to precompute similarity matrix
-def precompute_similarity_matrix(music_df):
-    features = music_df[['danceability', 'energy', 'key', 'loudness', 'speechiness', 'acousticness', 'instrumentalness', 'liveness', 'valence', 'tempo']].values
-    return cosine_similarity(MinMaxScaler().fit_transform(features))
-
-# Function for content-based recommendations
-def content_based_recommendations(input_song_name, music_df, similarity_matrix, num_recommendations=5):
-    if input_song_name not in music_df['Track Name'].values:
-        st.error(f"'{input_song_name}' not found in the dataset.")
+    if response.status_code == 200:
+        return response.json()['access_token']
+    else:
+        st.error(f"Error obtaining access token: {response.status_code} - {response.text}")
         return None
-    
-    input_song_index = music_df[music_df['Track Name'] == input_song_name].index[0]
-    similar_songs = music_df.iloc[similarity_matrix[input_song_index].argsort()[::-1][1:num_recommendations+1]]
 
-    return similar_songs[['Track Name', 'Artists', 'Album Name', 'Release Date', 'Popularity', 'Album Image URL', 'Genre']]
-
-# Function to get similar artists for the input song's artist
-def get_similar_artists(artist_id, sp):
+# Function to get track data from a song URL
+def get_song_data_from_url(song_url, access_token):
+    sp = spotipy.Spotify(auth=access_token)
     try:
-        similar_artists = sp.artist_related_artists(artist_id)['artists']
-        artist_data = []
-        for artist in similar_artists[:5]:  # Limit to top 5 similar artists
-            artist_info = {
-                'Artist Name': artist['name'],
-                'Genres': ', '.join(artist['genres']) if artist['genres'] else 'Unknown',
-                'Popularity': artist['popularity'],
-                'Followers': artist['followers']['total'],
-                'Image URL': artist['images'][0]['url'] if artist['images'] else None
-            }
-            artist_data.append(artist_info)
-        return artist_data
+        track_id = song_url.split('/')[-1].split('?')[0]  # Extract track ID from URL
+        track_info = sp.track(track_id)
+        return track_info
     except Exception as e:
-        st.error(f"Error fetching similar artists: {e}")
+        st.error(f"Error fetching song data: {e}")
+        return None
+
+# Function to get playlist data from a playlist URL
+def get_playlist_tracks_from_url(playlist_url, access_token):
+    sp = spotipy.Spotify(auth=access_token)
+    try:
+        playlist_id = playlist_url.split('/')[-1].split('?')[0]  # Extract playlist ID from URL
+        playlist_info = sp.playlist(playlist_id)  # Get full playlist info
+        tracks = [track['track'] for track in playlist_info['tracks']['items'] if track['track'] is not None]  # Return valid tracks
+        return playlist_info['name'], playlist_info['images'][0]['url'], tracks  # Return playlist name, image, and tracks
+    except Exception as e:
+        st.error(f"Error fetching playlist data: {e}")
+        return None, None, []
+
+# Function to get artist genres
+def get_artist_genres(artist_id, sp):
+    try:
+        artist_info = sp.artist(artist_id)
+        return artist_info.get('genres', [])[:3]  # Limit to 3 genres
+    except Exception as e:
+        st.error(f"Error fetching artist genres: {e}")
         return []
 
-# Streamlit app - add artist recommendations section
-st.title("Spotify Playlist Song Recommender")
+# Function to get similar songs
+def get_similar_songs(track_id, sp):
+    try:
+        recommendations = sp.recommendations(seed_tracks=[track_id], limit=10)
+        return recommendations['tracks']
+    except Exception as e:
+        st.error(f"Error fetching similar songs: {e}")
+        return []
 
-playlist_id = st.text_input("Enter Spotify Playlist ID", "")
-if playlist_id:
-    access_token = get_access_token()
-    if access_token:
-        # Initialize Spotify API client
-        sp = spotipy.Spotify(auth=access_token)
-        
-        music_df = get_trending_playlist_data(playlist_id, access_token)
-        similarity_matrix = precompute_similarity_matrix(music_df)
+# Streamlit app
+st.title("Spotify Recommendation System")
 
-        st.write("Playlist Data")
-        st.dataframe(music_df[['Track Name', 'Artists', 'Album Name', 'Popularity', 'Genre', 'Release Date']])
+input_type = st.radio("Select Input Type", ("Song", "Playlist"))
 
-    input_song_name = st.text_input("Enter a song from the playlist for recommendations")
-    if input_song_name:
-        # Content-based song recommendations
-        recommendations = content_based_recommendations(input_song_name, music_df, similarity_matrix)
-        if recommendations is not None:
-            st.write(f"Recommended music similar to '{input_song_name}':")
-            for _, row in recommendations.iterrows():
-                st.image(row['Album Image URL'], width=150)
-                st.write(f"**Track Name**: {row['Track Name']}")
-                st.write(f"**Artists**: {row['Artists']}")
-                st.write(f"**Album Name**: {row['Album Name']}")
-                st.write(f"**Genre**: {row['Genre']}")
-                st.write(f"**Popularity**: {row['Popularity']}")
-                st.write(f"**Release Date**: {row['Release Date']}")
+if input_type == "Song":
+    song_url = st.text_input("Enter Song URL", "")
+    if song_url:
+        access_token = get_access_token()
+        if access_token:
+            # Initialize Spotify API client
+            sp = spotipy.Spotify(auth=access_token)
+            st.write("---")
+            # Fetch song data from the provided URL
+            track_info = get_song_data_from_url(song_url, access_token)
+            if track_info:
+                track_id = track_info['id']
+                track_name = track_info['name']
+                track_artists = ', '.join([artist['name'] for artist in track_info['artists']])
+                track_genres = ', '.join(get_artist_genres(track_info['artists'][0]['id'], sp))
+
+                st.subheader("**You searched for:**")
+                st.image(track_info['album']['images'][0]['url'], width=150)
+                st.write(f"**Track Name**: {track_name}")
+                st.write(f"**Artists**: {track_artists}")
+                st.write(f"**Genres**: {track_genres}")
                 st.write("---")
 
-            # Artist recommendation section
-            st.write(f"Artists you might like based on '{input_song_name}'")
+                # Get similar songs
+                similar_songs = get_similar_songs(track_id, sp)
+                if similar_songs:
+                    st.subheader("**Recommended Songs**:")
+                    cols = st.columns(5)  # Change to 5 columns
+                    for i, song in enumerate(similar_songs):
+                        with cols[i % 5]:  # Arrange 2 songs per row
+                            st.image(song['album']['images'][0]['url'], width=150)
+                            st.write(f"**Track Name**: {song['name']}")
+                            st.write(f"**Artists**: {', '.join([artist['name'] for artist in song['artists']])}")
+                            song_genres = ', '.join(get_artist_genres(song['artists'][0]['id'], sp))  # Get genres for the song
+                            st.write(f"**Genres**: {song_genres}")
+                            st.write(f"[Listen here]({song['external_urls']['spotify']})")
+                st.write("---")
 
-            # Get the primary artist of the input song
-            input_song = music_df[music_df['Track Name'] == input_song_name].iloc[0]
-            primary_artist_name = input_song['Artists'].split(",")[0]
-            
-            # Get artist ID from Spotify
-            artist_results = sp.search(q=f'artist:{primary_artist_name}', type='artist')
-            if artist_results['artists']['items']:
-                primary_artist_id = artist_results['artists']['items'][0]['id']
-                similar_artists = get_similar_artists(primary_artist_id, sp)
-                
+                # Get similar artists
+                primary_artist_id = track_info['artists'][0]['id']
+                similar_artists = sp.artist_related_artists(primary_artist_id)['artists'][:8]  # Limit to 10 artists
                 if similar_artists:
-                    for artist in similar_artists:
-                        st.image(artist['Image URL'], width=150)
-                        st.write(f"**Artist Name**: {artist['Artist Name']}")
-                        st.write(f"**Genres**: {artist['Genres']}")
-                        st.write(f"**Popularity**: {artist['Popularity']}")
-                        st.write(f"**Followers**: {artist['Followers']}")
+                    st.subheader("**Artists you may like**:")
+                    cols_artists = st.columns(5)  # Change to 5 columns
+                    for i, artist in enumerate(similar_artists):
+                        with cols_artists[i % 5]:  # Arrange 2 artists per row
+                            st.image(artist['images'][0]['url'], width=150)
+                            st.write(f"**Artist Name**: {artist['name']}")
+                            st.write(f"**Genres**: {', '.join(get_artist_genres(artist['id'], sp))}")  # Limit to 3 genres
+                            st.write(f"[Listen here]({artist['external_urls']['spotify']})")
+                st.write("---")
+
+elif input_type == "Playlist":
+    playlist_url = st.text_input("Enter Playlist URL", "")
+    if playlist_url:
+        access_token = get_access_token()
+        if access_token:
+            # Initialize Spotify API client
+            sp = spotipy.Spotify(auth=access_token)
+            st.write("---")
+            # Fetch playlist data from the provided URL
+            playlist_name, playlist_image, playlist_tracks = get_playlist_tracks_from_url(playlist_url, access_token)
+            if playlist_tracks:
+                # Show the playlist image and name
+                st.subheader("**You searched for:**")
+                st.image(playlist_image, width=150)
+                st.write(f"**Name**: {playlist_name}")
+                st.write("---")
+
+                # Create a dataframe to display playlist tracks
+                track_data = {
+                    "Track Name": [track['name'] for track in playlist_tracks],
+                    "Artists": [', '.join([artist['name'] for artist in track['artists']]) for track in playlist_tracks],
+                    "Genres": [', '.join(get_artist_genres(track['artists'][0]['id'], sp)) for track in playlist_tracks],
+                }
+                track_df = pd.DataFrame(track_data)
+
+                # Show the playlist tracks in a table
+                st.subheader("**Playlist Tracks**:")
+                st.dataframe(track_df)
+
+                selected_track = st.selectbox("Select a track from the playlist", options=track_df.index, format_func=lambda x: track_df['Track Name'][x])
+
+                if selected_track is not None:
+                    # Get selected track information
+                    selected_track_info = playlist_tracks[selected_track]
+                    selected_track_id = selected_track_info['id']
+                    selected_track_name = selected_track_info['name']
+                    selected_track_artists = ', '.join([artist['name'] for artist in selected_track_info['artists']])
+                    selected_track_genres = ', '.join(get_artist_genres(selected_track_info['artists'][0]['id'], sp))
+
+                    st.subheader("**You selected**:")
+                    st.image(selected_track_info['album']['images'][0]['url'], width=150)
+                    st.write(f"**Track Name**: {selected_track_name}")
+                    st.write(f"**Artists**: {selected_track_artists}")
+                    st.write(f"**Genres**: {selected_track_genres}")
+                    st.write("---")
+
+                    # Get similar songs for the selected track
+                    similar_songs = get_similar_songs(selected_track_id, sp)
+                    if similar_songs:
+                        st.subheader("**Recommended Songs**:")
+                        cols = st.columns(4)  # Change to 5 columns
+                        for i, song in enumerate(similar_songs):
+                            with cols[i % 4]:  # Arrange 2 songs per row
+                                st.image(song['album']['images'][0]['url'], width=150)
+                                st.write(f"**Track Name**: {song['name']}")
+                                st.write(f"**Artists**: {', '.join([artist['name'] for artist in song['artists']])}")
+                                song_genres = ', '.join(get_artist_genres(song['artists'][0]['id'], sp))  # Get genres for the song
+                                st.write(f"**Genres**: {song_genres}")
+                                st.write(f"[Listen here]({song['external_urls']['spotify']})")
                         st.write("---")
-            else:
-                st.error(f"No artist found for '{primary_artist_name}'")
+                
+                    # Get similar artists based on the selected track's primary artist
+                    primary_artist_id = selected_track_info['artists'][0]['id']
+                    similar_artists = sp.artist_related_artists(primary_artist_id)['artists'][:8]  # Limit to 10 artists
+                    if similar_artists:
+                        st.subheader("**Suggested Artists**:")
+                        cols_artists = st.columns(4)  # Change to 5 columns
+                        for i, artist in enumerate(similar_artists):
+                            with cols_artists[i % 4]:  # Arrange 2 artists per row
+                                st.image(artist['images'][0]['url'], width=150)
+                                st.write(f"**Artist Name**: {artist['name']}")
+                                st.write(f"**Genres**: {', '.join(get_artist_genres(artist['id'], sp))}")  # Limit to 3 genres
+                                st.write(f"[Listen here]({artist['external_urls']['spotify']})")
+            st.write("---")
